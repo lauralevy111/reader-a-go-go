13ff82ec044cf46317eadbaf38c4ad5e47847ed2
Laura Levy <llevy510@gmail.com>
2020-06-28 10:39:27 -0400

    compare wahts up case created w standard output

--- main.go
+++ main.go
@@ -29,12 +29,16 @@ func main() {
     }
     if strings.Compare("help",text)==true {
       fmt.Println("all you need is within you now!")
-      fmt.Println(strings.Contains("help",text))
+      fmt.Println(strings.Compare("help",text))
     }
 
     if strings.Compare("hey",text)>=0 {
       fmt.Println("Hello, yourself")
       fmt.Println(strings.Compare("help",text))
+    }
+
+    if strings.Compare("whats up?",text)==true {
+      fmt.Println("you wanna know whats UP???")
     }
 
   }
